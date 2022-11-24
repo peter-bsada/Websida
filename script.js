4404c5761a5d94df877409fa12cfe7254a9d38e0
peter-bsada <57670991+peter-bsada@users.noreply.github.com>
2022-11-24 11:23:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var header = document.getElementsByClassName("nav-menu");
+var btns = document .getElementsByClassName("btn");
+
+
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
